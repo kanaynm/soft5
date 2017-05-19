887a097f0aaecf018757fbe38ee22538affc591e
g15957yk <g15957yk@s30428.tsuda.ac.jp>
2017-05-19 11:51:26 +0900

    ドライバー矢沼 ナビゲーター高澤 3桁の数字の英訳

--- IntToEng/src/IntToEng/IntToEng.java
+++ IntToEng/src/IntToEng/IntToEng.java
@@ -16,7 +16,11 @@ public class IntToEng {
 		int h=n/100;
 		int z=(n-h*100)/10;
 		int i=n-(h*100+z*10);
-		System.out.println("i="+judge1(i));
+		if(z==1){
+		System.out.println(judge1(h)+" hundred "+judge10(z,i));
+		} else {
+		System.out.println(judge1(h)+" hundred "+judge10(z,i)+" "+judge1(i));
+		}
         return "";
     }
 	static String judge1(int i){
@@ -31,28 +35,29 @@ public class IntToEng {
 		else if(i==9)return "nine";
 		return "";
 	}
-	static String judge10(int i){
-		if(i==1)return "ten";
+	static String judge10(int i,int j ){
+		if(i==1)return judgeteen(j);
 		else if(i==2)return "twenty";
-		else if(i==3)return "t";
-		else if(i==4)return "four";
-		else if(i==5)return "five";
-		else if(i==6)return "six";
-		else if(i==7)return "seven";
-		else if(i==8)return "eight";
-		else if(i==9)return "nine";
+		else if(i==3)return "thirty";
+		else if(i==4)return "fourty";
+		else if(i==5)return "fifty";
+		else if(i==6)return "sixty";
+		else if(i==7)return "seventy";
+		else if(i==8)return "eightty";
+		else if(i==9)return "ninety";
 		return "";
 	}
-	static String judge100(int i){
-		if(i==1)return "ten";
-		else if(i==2)return "twenty";
-		else if(i==3)return "t";
-		else if(i==4)return "four";
-		else if(i==5)return "five";
-		else if(i==6)return "six";
-		else if(i==7)return "seven";
-		else if(i==8)return "eight";
-		else if(i==9)return "nine";
-		return "";
+	static String judgeteen(int i){
+		if(i==1)return "eleven";
+	else if(i==2)return "twelve";
+	else if(i==3)return "thirteen";
+	else if(i==4)return "fourteen";
+	else if(i==5)return "fifteen";
+	else if(i==6)return "sixteen";
+	else if(i==7)return "seventeen";
+	else if(i==8)return "eighteen";
+	else if(i==9)return "nineteen";
+	else return "ten";
+	
 	}
 }
